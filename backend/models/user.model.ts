4eb9ba9eda5b271fac30004a13a7984abca97aa1
CosmicSpectrum <hareldaganh@gmail.com>
2022-11-26 00:12:37 +0200

    user modle is ready with the middleware and the encryption password

--- backend/models/user.model.ts
+++ backend/models/user.model.ts
@@ -1,16 +1,6 @@
-import { model, Schema, Model, Document } from 'mongoose';
+import { model, Schema } from 'mongoose';
 const bcrypt = require('bcrypt');
 
-
-interface IUser extends Document {
-    firstName: string;
-    lastName: string;
-    username: string;
-    password: string;
-    emailAddress: string;
-    homeAddress: string;
-    role: number;
-}
 
 const UserSchema: Schema = new Schema({
     firstName: {type: String, required: true},
@@ -22,6 +12,31 @@ const UserSchema: Schema = new Schema({
     role: {type: Number, required: true}  
 })
 
+//A middleware that hashs and updates the user password
+UserSchema.pre('save', function(next): void{
+    var user = this;
+    
+    if(!user.isModified('password')) return next();
 
+    bcrypt.getSalt(process.env.SALT_WORK_FACTOR, (err: Error, salt: string)=>{
+        if(err) return next(err);
 
-export const User: Model<IUser> = model('user',UserSchema);
+        bcrypt.hash(user.password, salt, (err: Error, hash: string)=>{
+            if(err) return next(err);
+
+            user.password = hash;
+            next();
+        })
+    })
+})
+
+//Password validation method added to the schema.
+UserSchema.methods.comparePassword = function(testedPassword: string, callback: Function){
+    bcrypt.compare(testedPassword, this.password, (err: Error, isMatch: Boolean)=>{
+        if(err) return callback(err);
+
+        callback(null, isMatch);
+    })
+}
+
+export default model('user',UserSchema);
